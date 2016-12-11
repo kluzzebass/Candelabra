6f6bb9d37009e7c5b9073e87ef559b9d259fc5aa
Jan Fredrik Leversund <kluzz@radical.org>
2016-12-11 23:48:42 +0100

    Added support buttons and brightness levels.

--- src/Candelabra.cpp
+++ src/Candelabra.cpp
@@ -6,12 +6,45 @@ Candelabra::Candelabra()
 
 }
 
-void setup()
+void Candelabra::setup()
 {
+	pinMode(BUTTON1_PIN, INPUT_PULLUP);
+	pinMode(BUTTON2_PIN, INPUT_PULLUP);
+	btn1.delay = DEBOUNCE_DELAY;
+	btn2.delay = DEBOUNCE_DELAY;
 
+	Serial.begin(115200);
+
+	strip.begin();
+	strip.show();
 }
 
-void loop()
+void Candelabra::loop()
 {
-	
+	btn1.update(!digitalRead(BUTTON1_PIN));
+	btn2.update(!digitalRead(BUTTON2_PIN));
+
+	if (btn1.changed || btn2.changed)
+	{
+		Serial.print(F("Buttons: "));
+		Serial.print(btn1.state);
+		Serial.print(F(", "));
+		Serial.println(btn2.state);
+	}
+
+	if (btn1.changed && btn1.state)
+		changeBrightness();
+}
+
+void Candelabra::changeBrightness()
+{
+	brightness = (brightness + 1) % BRIGHTNESS_LEVELS;
+
+	uint8_t level = pgm_read_byte_near(brightnessLevels + brightness);
+	strip.setBrightness(level);
+	Serial.print(F("Brightness set to level: "));
+	Serial.print(brightness);
+	Serial.print(F(" ("));
+	Serial.print(level);
+	Serial.println(F(")"));
 }
--- src/Candelabra.h
+++ src/Candelabra.h
@@ -2,14 +2,65 @@
 #ifndef CANDELABRA_H
 #define CANDELABRA_H
 
+#include <Arduino.h>
 #include <Debouncer.h>
+#include <Adafruit_NeoPixel.h>
+#include <avr/pgmspace.h>
+#include <avr/power.h>
+
+#define BUTTON1_PIN 9
+#define BUTTON2_PIN 10
+#define STRIP_PIN 6
+#define LED_COUNT 33
+#define DEBOUNCE_DELAY 1000
+#define BRIGHTNESS_LEVELS 9
+
+const uint8_t brightnessLevels[BRIGHTNESS_LEVELS] PROGMEM = {
+	255, 223, 191, 159, 127, 95, 63, 31, 0
+};
+
+const uint8_t seq1[LED_COUNT] PROGMEM = {
+	0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32 
+};
+
+const uint8_t seq2[LED_COUNT] PROGMEM = {
+	0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32,
+	1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31
+};
+
+const uint8_t seq3[LED_COUNT] PROGMEM = {
+	1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31,
+	0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32
+};
+
+const uint8_t seq4[LED_COUNT] PROGMEM = {
+	0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32,
+	31, 29, 27, 25, 23, 21, 19, 17, 15, 13, 11, 9, 7, 5, 3, 1
+};
+
+const uint8_t seq5[LED_COUNT] PROGMEM = {
+	31, 29, 27, 25, 23, 21, 19, 17, 15, 13, 11, 9, 7, 5, 3, 1,
+	0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32
+};
+
 
 class Candelabra
 {
+public:
 	Candelabra();
 
 	void setup();
 	void loop();
+
+private:
+	Debouncer btn1 = Debouncer();
+	Debouncer btn2 = Debouncer();
+	Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_COUNT, STRIP_PIN, NEO_GRB + NEO_KHZ800);
+	uint8_t brightness = 0;
+
+	void changeBrightness();
+
+
 };
 
 
